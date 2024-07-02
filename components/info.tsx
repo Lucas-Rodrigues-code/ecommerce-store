a45a3548e16cae51c07f4d95c5fe40d2871264dd
lucas <Rodriguesdesouzal28@gmail.com>
2024-07-02 11:16:29 -0300

    chore: Enable add to cart button in product info component

--- components/info.tsx
+++ components/info.tsx
@@ -5,11 +5,17 @@ import { ShoppingCart } from "lucide-react";
 import { Product } from "@/types";
 import Currency from "./ui/currency";
 import Button from "./ui/button";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
 }
 export default function Info({ data }: InfoProps) {
+  const cart = useCart();
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data?.name}</h1>
@@ -33,7 +39,7 @@ export default function Info({ data }: InfoProps) {
         </div>
       </div>
       <div className="mt-10 flex - items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Adionar ao carrinho
           <ShoppingCart />
         </Button>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,6 +13,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
            font-semibold hover:opacity-75 transition`,
           className
         )}
+        disabled={disabled}
         ref={ref}
         {...props}
       >
